bd0b9f5358af8e30146bad0f89dc63d72f8c0a91
Andrew Hayward <github@andrewhayward.net>
2026-08-01 19:16:33 +0100

    Updating rebuild function

--- netlify/functions/rebuild.mjs
+++ netlify/functions/rebuild.mjs
@@ -1,11 +1,8 @@
 const BUILD_HOOK = "https://api.netlify.com/build_hooks/6a6e1b9b9f15e873079eed94";
 
 export default async (req) => {
-    await fetch(BUILD_HOOK, { method: 'POST' }).then(response => {
-        console.log('Build hook response:', response)
+    return await fetch(BUILD_HOOK, { method: 'POST' }).then(response => {
+        console.log('Build hook response:', response);
+        return response;
     });
-
-    return {
-        statusCode: 200
-    };
 }
