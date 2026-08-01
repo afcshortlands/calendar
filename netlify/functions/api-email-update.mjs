fd5523ecb6f4407a54e33ff08f513de3ae97f0c0
Andrew Hayward <github@andrewhayward.net>
2026-08-01 22:15:33 +0100

    Checking auth header properly

--- netlify/functions/api-email-update.mjs
+++ netlify/functions/api-email-update.mjs
@@ -3,7 +3,7 @@ const BUILD_HOOK = "https://api.netlify.com/build_hooks/6a6e1b9b9f15e873079eed94
 export const config = { path: "/api/email-update" };
 
 export default async (req) => {
-    const auth = req.headers.authorization;
+    const auth = await req.headers.get("Authorization");
     const payload = await req.json();
     console.log(auth, payload);
 
